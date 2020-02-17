4efef053a4eb9295f61107232e5f04167ac0a9da
Kayla Wood <kaylaewood@gmail.com>
2020-02-17 14:34:03 -0700

    Add and delete todos

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddToDo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,15 +11,28 @@ export default function App() {
     {text: 'play on the switch', key: '3'},
   ])
 
+  const pressHandler = key => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = text => {
+    setTodos((prevTodos) => {
+      return [{text: text, key: Math.random().toString()}, ...prevTodos];
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
+        <AddToDo submitHandler={submitHandler}/>
         <View style={styles.list}>
           <FlatList
             data={todos}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              <TodoItem item={item} pressHandler={pressHandler}/>
             )}
           />
         </View>
---
+++ components/addTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+
+export default function AddToDo({ submitHandler }) {
+  const [text, setText] = useState('');
+
+  const changeHandler = value => {
+    setText(value);
+  }
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder='New todo...'
+        onChangeText={changeHandler}
+        //same as value => changeHandler(value)
+      />
+      <Button
+        onPress={() => submitHandler(text)}
+        title='add todo'
+        color='coral'
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd'
+  }
+})
